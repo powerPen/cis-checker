05785fefc534bbf4d55cc3d77106769f81b8769d
powerPen <powerpen@difisy.net>
2017-01-23 18:44:48 +0000

    Create cis-checker.ps1

---
+++ cis-checker.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+Parse-Nessus produces a directory of standalone html reports from a .nessus file.
+.DESCRIPTION
+
+	Operating System	Benchmark Version
+	- Windows 7		 2.1.0
+	- Windows 2003	  2.0
+	- Windows 2008	  2.1.0
+	- Windows 2008R2	2.1.0
+	- Windows 2012	  1.0
+	- Windows 2012R2	2.1.0
+
+
+.EXAMPLE
+
+.NOTES
+Author: 		
+Last Modified:	
+#>
